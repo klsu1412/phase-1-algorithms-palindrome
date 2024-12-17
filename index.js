ec6455b9120ff8f43304d15455818caed561cbf0
Kenn Su <kliusu1412@gmail.com>
2024-12-17 09:13:16 -0500

    adds functionality to the isPalindrome function to check if an input word is a palindrome or not

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reversedWord = reverseWord(word)
+
+  if(word === reversedWord) {
+    return true
+  } else {
+    return false
+  }
+} 
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  make a reverse of the input string
+  check if the reversed string is the same
+  return true if it is
+  return false if it is not
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  The isPalindrone function calls the reverseWord function to 
+  reverse the input and then compares the reversed input with the
+  input and if it is the same then it returns a value of true, and
+  if it is not the same then it returns a value of false.
 */
+
+function reverseWord(word) {
+  const array = word.split("")
+  const reversedArray = array.reverse()
+  const reversedWord = reversedArray.join("")
+  return reversedWord
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
